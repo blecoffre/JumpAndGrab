573330120735257b3dc55b3a2f4d92fb623e6485
blecoffre <bertrandlecoffre@outlook.com>
2020-02-03 16:39:20 +0100

    Some scripts

---
+++ JumpAndGrab/Assets/Scripts/Player/CustomInputs.cs
@@ -0,0 +1,275 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Player/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @CustomInputs : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @CustomInputs()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""19dda618-499b-4dae-9772-285c9a1854f2"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""b9abadc3-96e8-4046-9e72-4a89d92d7a51"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Direction"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""e6b0062a-681c-493f-b578-1a9e470967c7"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""ZQSD"",
+                    ""id"": ""612a2bdf-02de-4076-a53d-5bdf38daf04a"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fbc34b48-eae1-434b-a989-f4b5c0bd943a"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""f491caf0-0ea0-416c-b36e-d1192f56ddc0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""4369fc2c-4008-4e49-b4a6-024d2566360f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""9d292aaf-cd9f-44ad-9e86-7bf45ccede42"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""84b1330e-509c-464b-935a-d5908872e98c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Direction"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""5a338a2f-010f-4674-a156-7ab5bf97291b"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Direction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""6be34554-0bee-4a11-805f-36db95a88471"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Direction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c644861f-dc20-4584-b52c-78d8951a8abc"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Direction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8c812130-28f4-4e4d-8510-b8c928e9a72c"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Direction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and Mouse"",
+            ""bindingGroup"": ""Keyboard and Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Direction = m_Player.FindAction("Direction", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Direction;
+    public struct PlayerActions
+    {
+        private @CustomInputs m_Wrapper;
+        public PlayerActions(@CustomInputs wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Direction => m_Wrapper.m_Player_Direction;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Direction.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDirection;
+                @Direction.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDirection;
+                @Direction.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDirection;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Direction.started += instance.OnDirection;
+                @Direction.performed += instance.OnDirection;
+                @Direction.canceled += instance.OnDirection;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardandMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandMouseSchemeIndex == -1) m_KeyboardandMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and Mouse");
+            return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnDirection(InputAction.CallbackContext context);
+    }
+}
---
+++ JumpAndGrab/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private Rigidbody m_rigidbody;
+
+    private CustomInputs controls;
+
+    public float MoveSpeed = 15;
+    public float RotationSpeed = 5;
+    Vector3 inputDirection;
+    private Vector3 movement;
+
+    private Vector2 movementInput;
+    Vector2 lookPosition;
+
+    private void Awake()
+    {
+        controls = new CustomInputs();
+        controls.Player.Movement.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        controls.Player.Direction.performed += ctx => lookPosition = ctx.ReadValue<Vector2>();
+
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void LateUpdate()
+    {
+        float h = movementInput.x;
+        float v = movementInput.y;
+
+        var targetInput = new Vector3(h, 0, v);
+        inputDirection = Vector3.Lerp(inputDirection, targetInput, Time.deltaTime * 10f);
+
+        //Camera Direction
+        var cameraForward = mainCamera.transform.forward;
+        var cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        //Try not to use var for roadshows or learning code
+        Vector3 desiredDirection = cameraForward * inputDirection.z + cameraRight * inputDirection.x;
+
+        //Why not just pass the vector instead of breaking it up only to remake it on the other side?
+        MoveThePlayer(desiredDirection);
+        TurnThePlayer();
+    }
+
+    void MoveThePlayer(Vector3 desiredDirection)
+    {
+        movement.Set(desiredDirection.x, 0f, desiredDirection.z);
+
+        movement = movement * MoveSpeed * Time.deltaTime;
+
+        m_rigidbody.MovePosition(transform.position + movement);
+
+    }
+
+    void TurnThePlayer()
+    {
+
+        // Old Input system
+        Vector2 input = lookPosition;
+
+        // Convert "input" to a Vector3 where the Y axis will be used as the Z axis
+        Vector3 lookDirection = new Vector3(input.x, 0, input.y);
+        var lookRot = mainCamera.transform.TransformDirection(lookDirection);
+        lookRot = Vector3.ProjectOnPlane(lookRot, Vector3.up);
+
+        if (lookRot != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(lookRot);
+            m_rigidbody.MoveRotation(newRotation);
+        }
+    }
+}
