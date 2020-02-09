2e55cad858db7ba9646409c269f4487b3a3d2843
blecoffre <bertrandlecoffre@outlook.com>
2020-02-09 01:33:02 +0100

    Create NullCheckExtension

---
+++ JumpAndGrab/Assets/Scripts/NullCheckExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class NullCheckExtension
+{
+    public static void InvokeIfNotNull(this UnityAction action)
+    {
+        if (action != null)
+            action.Invoke();
+    }
+
+    public static void SetActiveChecked(this GameObject gameObject, bool active)
+    {
+        if (gameObject != null && gameObject.activeSelf != active)
+            gameObject.SetActive(active);
+    }
+}
