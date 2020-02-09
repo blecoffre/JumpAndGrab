387110d02dc51f2d533d72cd190ae42ee39454e1
blecoffre <bertrandlecoffre@outlook.com>
2020-02-09 01:34:06 +0100

    GameController

---
+++ JumpAndGrab/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private CustomInputs controls;
+
+    [SerializeField]
+    private InGameMenuViewController m_ingameMenuViewController;
+
+    [SerializeField]
+    private PlayerController player;
+
+    void Awake()
+    {
+        controls = new CustomInputs();
+        player.controls = controls;
+        controls.ExtraControls.OpenMenu.performed += ctx => FlipFlopMainMenu();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void FlipFlopMainMenu()
+    {
+        Debug.Log("CC");
+        m_ingameMenuViewController.FlipFlopMainMenu();
+    }
+}
